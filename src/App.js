2bf7f8f734c142e36424874f0bb869869b7a8dfb
Katarzyna Bogusz <kabogusz@interia.pl>
2024-01-02 19:32:46 +0100

    Add files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Timer from './Components/Timer/Timer';
+import './styles/global.css';
+function App() {
+  return (
+    <div className='app'>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
